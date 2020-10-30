0d9dedae73becb80a78a423606a82f295eae15ef
Ali Han Özdoğan <alihanozdogan@eskisehir.edu.tr>
2020-10-30 16:01:41 +0300

    easy-co

---
+++ reading tablets/Assets/scripts/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour {
+ [SerializeField]//or use public in next line
+ Text Result_Display;
+ string InputString;
+ int[] arg=new int[2] {0,0};
+ int i=0;
+ string operation;
+ int answer,x;
+ bool nextRun=true;
+
+ public void ButtonPressed(){
+  if (nextRun) {
+   InputString = "";
+   nextRun = false;
+  }
+  //Debug.Log (EventSystem.current.currentSelectedGameObject.name); 
+  string ButtValue = EventSystem.current.currentSelectedGameObject.name;
+  InputString += ButtValue;
+  Result_Display.text = InputString;
+
+  if (int.TryParse (ButtValue, out x)) {
+   arg [i] = (arg [i] * 10) + int.Parse (ButtValue);
+   //Result_Display.text = arg [i].ToString ();
+  } 
+  else {
+   if (ButtValue != "=") {
+    operation = ButtValue;
+    i++;
+   } else {
+    switch (operation) {
+     case "+":
+     answer = arg [0] + arg [1];
+     break;
+    case "-":
+     answer = arg [0] - arg [1];
+     break;
+    case "*":
+     answer = arg [0] * arg [1];
+     break;
+    case "/":
+     answer = arg [0] / arg [1];
+     break;
+    }
+
+    Result_Display.text = answer.ToString();
+    arg [0] = 0;
+    arg [1] = 0;
+    i = 0;
+    nextRun = true;
+   }
+  }
+
+ }
+
+}
---
+++ reading tablets/Assets/scripts/keyboard2.cs
@@ -0,0 +1,93 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+using UnityEditor;
+
+public class keyboard2 : MonoBehaviour
+{
+    private const string Path = "test";
+    public Text text;
+    public string word = null;
+    int wordIndex = 0;
+    string alpha = null;
+    string alpha2 = null;
+    char[] nameChar = new char[1000];
+    public Text index = null;
+    public Text Text = null;
+    string path = "Assets/word.txt";
+
+    void Start(){
+        wordSaver();
+            
+    }
+      
+     public void alphabetFunction(string alphabet)
+    {
+        
+        wordIndex++;
+        char[] keepchar = alphabet.ToCharArray();
+        nameChar[wordIndex] = keepchar[0];
+        alpha = nameChar[wordIndex].ToString();
+        word += alphabet;
+        Text.text = word;
+        index.text = wordIndex.ToString();
+
+        
+        PlayerPrefs.SetString ("wordi2", word);
+        Debug.Log(word);
+       
+        }
+    public void backspace() {
+    
+        if (wordIndex >= 0 )
+        {
+            wordIndex--;
+            alpha2 = null;
+            for(int i = 1; i < wordIndex + 1; i++)
+            {
+                alpha2 += nameChar[i].ToString();
+
+            }
+            word = alpha2;
+            Text.text = word;
+            index.text = wordIndex.ToString();
+            
+        }
+    
+    }
+  
+    public void changescene()
+    {
+        SceneManager.LoadScene("a-h");
+            
+    }
+    public void changescene2()
+    {
+        SceneManager.LoadScene("r-z");
+     
+    }
+    public void changescene3()
+    {
+        SceneManager.LoadScene("ı-p");
+      
+    }
+    public void wordSaver(){
+        if(SceneManager.GetActiveScene().buildIndex > 0 && SceneManager.GetActiveScene().buildIndex < 5){
+        text.text = PlayerPrefs.GetString("wordi2") +word ;   
+        
+    }
+  
+    
+   
+}
+
+
+
+
+
+
+}
